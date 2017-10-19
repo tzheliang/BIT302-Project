002a5406d8660acb704ac62e00c5950148a59680
Zheliang Tan <zheliangtan@gmail.com>
2017-10-19 23:43:13 +0800

    PHP script to retrieve menu

--- retrieveMenu.php
+++ retrieveMenu.php
@@ -1,4 +1,6 @@
 <?php
+  session_start();
+
   $servername = "localhost";
   $name = "root";
   $password = "";
@@ -6,12 +8,19 @@
   $con = new mysqli($servername, $name, $password, $dbname);
 
   if (isset($_POST['submit'])) {
-    // $restaurantID = $_POST['submit'];
-    // $sql = "SELECT * FROM menuitem WHERE restaurantID = '$restaurantID'";
-    // $result = mysqli_query($con, $sql);
-    $sql = "SELECT * FROM Restaurant";
+    $restaurantID = $_POST['submit'];
+    $sql = "SELECT * FROM menuitem where restaurantID = '$restaurantID'";
     $result = mysqli_query($con, $sql);
-    $_SESSION['abc'] = $result;
+    $_SESSION['arr'] = [];
+    foreach ($result as $row) {
+      echo $row['foodID'];
+      $_SESSION['arr'][] = $row;
+    }
+
+    $sql2 = "SELECT restaurantName FROM restaurant WHERE RestaurantID= '$restaurantID'";
+    $result2 = mysqli_query($con, $sql2);
+    $row = mysqli_fetch_assoc($result2);
+    $_SESSION['restaurantName'] = $row['restaurantName'];
     header("location: orders-list.html");
     mysqli_close($con);
   }
