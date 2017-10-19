caa5dc43b49538f36b9b4881ef378d3e73e2a87f
Zheliang Tan <zheliangtan@gmail.com>
2017-10-19 15:17:27 +0800

    Added SQL file to create sample data. (Still empty)

---
+++ db_files/sampledata.sql
@@ -0,0 +1,25 @@
+INSERT
+INTO
+  `users`(
+    `userID`,
+    `username`,
+    `PASSWORD`,
+    `firstName`,
+    `lastName`,
+    `email`,
+    `address`
+  )
+VALUES('', '', '', '', '', '', NULL)
+
+INSERT
+INTO
+  `restaurant`(
+    `restaurantID`,
+    `restaurantName`,
+    `location`,
+    `phoneNumber`,
+    `email`,
+    `image`,
+    `ownerID`
+  )
+VALUES('', '', '', '', '', '', NULL)
