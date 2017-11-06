2b46ada8d04a13a4e3ebfce17a9147975ebb5f7f
Wilson Ckm <ckm_5801@yahoo.com>
2017-11-06 23:12:03 +0800

    removed address attribute

--- ownerProfile.php
+++ ownerProfile.php
@@ -158,10 +158,6 @@
                   <td><?php echo $_SESSION['fullname']; ?></td>
                 </tr>
                 <tr>
-                  <td><b>Restaurant Address:</b></td>
-                  <td><?php echo $_SESSION['address']; ?></td>
-                </tr>
-                <tr>
                   <td><b>Business Email:</b></td>
                   <td><?php echo $_SESSION['email']; ?></td>
                 </tr>
