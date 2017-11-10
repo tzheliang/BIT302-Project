b6030412d08507bf626bb76ad41c55d0749eae0e
Wilson Ckm <ckm_5801@yahoo.com>
2017-11-10 16:41:48 +0800

    fixed add menu for owner

--- addFoodItem.php
+++ addFoodItem.php
@@ -8,6 +8,8 @@
   $dbname = "food4all";
   $con = new mysqli($servername, $username, $password, $dbname);
 
+  $restaurantID = $_SESSION['userID'];
+
   $sql = "SELECT * FROM MenuItem";
   $result = mysqli_query($con, $sql);
   // Initialising variables
@@ -18,23 +20,12 @@
     $price = $_POST['price'];
     $status = $_POST['status'];
 
-    function getRestaurantInfo($ownerID, $con) {
-      $sql3 = "SELECT restaurantID FROM restaurant where ownerID = '$ownerID'";
-      $result1 = mysqli_query($con, $sql3);
-      $row2 = mysqli_fetch_assoc($result1);
-      $restaurantArray = array('restaurantID'=> $row2['restaurantID']);
-      return $restaurantArray;
-    }
-
     if ($passed){
-      while ($row = mysqli_fetch_assoc($result)) {
-        $restaurantArray = getRestaurantInfo($row['ownerID'], $con);
-        $sql2 = "INSERT INTO MenuItem (foodID, foodName, price, status, restaurantID) VALUES('0000', '$foodName', '$price', '$status', '$restaurantArray['restaurantID']')";
-        if (mysqli_query($con, $sql2)){
-          header("location: owner-mainPage.html");
-        } else {
-          echo "Cannot perform query";
-        }
+      $sql2 = "INSERT INTO MenuItem (foodID, foodName, price, status, restaurantID) VALUES('0000', '$foodName', '$price', '$status', '$restaurantID')";
+      if (mysqli_query($con, $sql2)){
+        header("location: owner-mainPage.html");
+      } else {
+        echo "Cannot perform query";
       }
     }
   } else {
